2d786cd084236d680cf41ff2c862539469067da4
LUNASNOW(NR) <daygod2019@gmail.com>
2025-11-22 13:47:02 +0700

    Update Lab1l1.java

--- src/Lab1l1/Lab1l1.java
+++ src/Lab1l1/Lab1l1.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Lab1l1 {
     public static void main(String[]args) {
+        System.out.print("Program to add numbersr : + : ");
         Scanner Getvalue = new Scanner(System.in);
 
         System.out.print("add Number : ");
@@ -15,3 +16,4 @@ public class Lab1l1 {
         System.out.println("Sum : "+number1+" + "+number2+" = "+(number1+number2));
     }
 }
+
