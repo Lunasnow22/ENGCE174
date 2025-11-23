66d60ec5bae177e1a8ef6889d215addae77b2b26
LUNASNOW(NR) <daygod2019@gmail.com>
2025-11-23 12:19:02 +0700

    Update Lab1l7.java

--- src/Lab1l7/Lab1l7.java
+++ src/Lab1l7/Lab1l7.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 
 public class Lab1l7 {
     public static void main(String[] args) {
-    	Scanner SumNumber = new Scanner(System.in);
+    	Scanner Getvalue = new Scanner(System.in);
     	long sum = 0;
     	
     	System.out.print("add number [N] : ");
-    	int number1 = SumNumber.nextInt();
+    	int number1 = Getvalue.nextInt();
     	
     	for(int i = 0; i < number1; i++) {
     		System.out.print("number :"+(i+1)+": ");
-    		int number2 = SumNumber.nextInt();
+    		int number2 = Getvalue.nextInt();
     		sum += number2;
     	}
-    	SumNumber.close();
+    	Getvalue.close();
     	
     	System.out.print("Sum "+ number1 +" :"+ sum + ":");
     }
+
 }
