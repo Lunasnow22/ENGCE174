338ce05f952b556246558a28a76f678d1375cc2c
LUNASNOW(NR) <daygod2019@gmail.com>
2025-11-23 10:17:37 +0700

    Update Lab1l2.java

--- src/Lab1l2/Lab1l2.java
+++ src/Lab1l2/Lab1l2.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 
 public class Lab1l2 {
     public static void main(String[]args) {
-        Scanner SumNumber = new Scanner(System.in);
+        Scanner Getvalue = new Scanner(System.in);
 
         System.out.print("add Number : ");
-        float number1 = SumNumber.nextFloat();
+        float number1 = Getvalue.nextFloat();
 
         System.out.print("add Number : ");
-        float number2 = SumNumber.nextFloat();
+        float number2 = Getvalue.nextFloat();
 
         System.out.println("Sum : "+(number1+number2));
     }
