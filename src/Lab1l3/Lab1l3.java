400e64c3d4e2ce3e1e50bd11e853b18bbbc4cd27
LUNASNOW(NR) <daygod2019@gmail.com>
2025-11-23 10:28:15 +0700

    Update Lab1l3.java

--- src/Lab1l3/Lab1l3.java
+++ src/Lab1l3/Lab1l3.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 
 public class Lab1l3 {
     public static void main(String[]args) {
-        Scanner SumNumber = new Scanner(System.in);
-
+        Scanner Getvalue = new Scanner(System.in);
+        System.out.println("Program for separating even and odd numbers");
+        
         System.out.print("add Number : ");
-        int number1 = SumNumber.nextInt();
+        int number1 = Getvalue.nextInt();
 
         if (number1 % 2 == 0) {
-        	System.out.println("Even");
+        	System.out.println("[ Even ]");
         }
         else {
-        	System.out.println("Odd");
+        	System.out.println("[ Odd ]");
         }
     }
 }
